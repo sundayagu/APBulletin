17d0d4ce1e82cb7917265ac9b39688b7294612e4
gabbyTI <gabrielibenye@gmail.com>
2019-03-25 16:16:39 +0100

    editor

--- apbulletin/ckeditor/ckeditorf/config.js
+++ apbulletin/ckeditor/ckeditorf/config.js
@@ -6,26 +6,25 @@
 CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
-	 config.uiColor = '#AADC6E';
+	// config.uiColor = '#AADC6E';
 
-	config.toolbarGroups = [
-		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
-		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
-		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
-		{ name: 'forms', groups: [ 'forms' ] },
-		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
-		'/',
-		{ name: 'links', groups: [ 'links' ] },
-		{ name: 'insert', groups: [ 'insert' ] },
-		'/',
-		{ name: 'styles', groups: [ 'styles' ] },
-		{ name: 'colors', groups: [ 'colors' ] },
-		{ name: 'tools', groups: [ 'tools' ] },
-		{ name: 'others', groups: [ 'others' ] },
-		{ name: 'about', groups: [ 'about' ] }
-	];
-
-	config.removeButtons = 'About,ShowBlocks,Maximize,Form,Checkbox,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField,BidiLtr,BidiRtl,Language,Anchor,Save,NewPage,Preview,Templates,Scayt,CopyFormatting,RemoveFormat,CreateDiv,Flash,Iframe';
+	//config.toolbarGroups = [
+	//	{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
+	//	{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+	//	{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
+	//	{ name: 'forms', groups: [ 'forms' ] },
+	//	{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+	//	{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
+	//	'/',
+	//	{ name: 'links', groups: [ 'links' ] },
+	//	{ name: 'insert', groups: [ 'insert' ] },
+	//	'/',
+	//	{ name: 'styles', groups: [ 'styles' ] },
+	//	{ name: 'colors', groups: [ 'colors' ] },
+	//	{ name: 'tools', groups: [ 'tools' ] },
+	//	{ name: 'others', groups: [ 'others' ] },
+	//	{ name: 'about', groups: [ 'about' ] }
+	//];
+	//config.removeButtons = 'About,ShowBlocks,Maximize,Form,Checkbox,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField,BidiLtr,BidiRtl,Language,Anchor,Save,NewPage,Preview,Templates,Scayt,CopyFormatting,RemoveFormat,CreateDiv,Flash,Iframe';
 	
 };
